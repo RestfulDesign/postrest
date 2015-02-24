b4d966a89eb6f8dd4494f703963515b67768c15d
andelo <anders@kaerus.com>
2015-02-24 09:55:40 +0100

    reformated & added _database/_collection_schema  accessors

--- test/database.js
+++ test/database.js
@@ -2,36 +2,36 @@ var postrest;
 
 try {
     postrest = PostRest
-} catch (e) {
+} catch(e) {
     postrest = require('..')
 }
 
 describe("database", function () {
-    
+
     var db;
-    
+
     before(function (done) {
-		db = postrest('http://kaerus:kaerus@127.0.0.1:8080');
-	
-		db.database.create("test")
-			.end(function () {
-				done();
-			});
+        db = postrest('http://kaerus:kaerus@127.0.0.1:8080');
+
+        db.database.create("test")
+            .end(function () {
+                done();
+            });
     });
-    
+
     describe("delete & create", function () {
 
-		it('delete [test] database', function (done) {
-			db.database.delete('test')
-				.callback(done);
-		})
-		
-		it('create [test] database', function (done) {
-			db.database.create('test')
-				.callback(done);
-		})
-	});
-	
-	
-	
+        it('delete [test] database', function (done) {
+            db.database.delete('test')
+                .callback(done);
+        })
+
+        it('create [test] database', function (done) {
+            db.database.create('test')
+                .callback(done);
+        })
+    });
+
+
+
 });
--- test/document.js
+++ test/document.js
@@ -4,57 +4,61 @@ var csvParse = require('csv-parse');
 
 try {
     postrest = PostRest
-} catch (e) {
+} catch(e) {
     postrest = require('..')
 }
 
 describe("document", function () {
-    
+
     var db, opendata;
-    
+
     before(function (done) {
-		db = postrest('http://kaerus:kaerus@127.0.0.1:8080');
-		var odata = postrest('http://www.opengeocode.org');
-		
-		db.database.delete("document").end(function() {
-			console.log("dropping document");
-			db.database.create("document").end(function() {
-			
-				db.connection.path.base = 'document';
-				console.log("creating books collection");
-				
-				db.collection.create('books')
-					.then(function(){
-						return odata.get('/opendata/opendata.csv');
-					}).then(function(csv){
-						console.log("got data");
-						return loadData(csv);
-					}).callback(done);
-			});
-		});
-		
-		function loadData(csv){
-			csvParse.parse(csv,{columns:true},function(err,ret){
-				if(err) return err;
-				
-				opendata = ret;
-				console.log("loaded %d rows", ret.length);
-			});
-		}
+        db = postrest('http://kaerus:kaerus@127.0.0.1:8080');
+        var odata = postrest('http://www.opengeocode.org');
+
+        db.database.delete("document").end(function () {
+            console.log("dropping document");
+            db.database.create("document").end(function () {
+
+                db.connection.path.base = 'document';
+                console.log("creating books collection");
+
+                db.collection.create('books')
+                    .then(function () {
+                        return odata.get('/opendata/opendata.csv');
+                    }).then(function (csv) {
+                        console.log("got data");
+                        return loadData(csv);
+                    }).callback(done);
+            });
+        });
+
+        function loadData(csv) {
+            csvParse.parse(csv, {
+                columns: true
+            }, function (err, ret) {
+                if(err) return err;
+
+                opendata = ret;
+                console.log("loaded %d rows", ret.length);
+            });
+        }
     });
-    
+
     describe("create", function () {
 
-		it('create one', function (done) {
-			db.document.create({name:""})
-		})
-		
-		it('create [test] database', function (done) {
-			db.database.create('test')
-				.callback(done);
-		})
-	});
-	
-	
-	
+        it('create one', function (done) {
+            db.document.create({
+                name: ""
+            })
+        })
+
+        it('create [test] database', function (done) {
+            db.database.create('test')
+                .callback(done);
+        })
+    });
+
+
+
 });
